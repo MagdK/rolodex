285ecfba047419de68bf4ce1cfded4806d09278d
MagdK <kovacsmagda@gmail.com>
2022-06-21 15:17:20 +0200

    Remove parentheses in class

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
-import { Component } from 'react';
 import './App.css';
+import { Component } from 'react';
 
-class App extends Component() {
+class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello! My name is Magda.
+            Hello Magda.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button>Change name</button>
         </header>
       </div>
     );
