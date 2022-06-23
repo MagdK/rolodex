0328e1d445aba987b5fd135279035330a58f2974
MagdK <kovacsmagda@gmail.com>
2022-06-23 19:41:13 +0200

    Move searchfield to state

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
     console.log('constructor')
   }
@@ -27,11 +28,27 @@ class App extends Component {
 
   render() {
     console.log('render')
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField); 
+    })
+
     return (
       <div className="App">
-        <input className='search-box' type='serach' placeholder='Search monsters...' onChange={(event) => { console.log(event.target.value)}}/>
+        <input 
+          className='search-box' 
+          type='serach' 
+          placeholder='Search monsters...' 
+          onChange={(event) => { 
+            const searchField = event.target.value.toLocaleLowerCase();
+
+            this.setState(() => {
+              return { searchField }
+            })
+          }}
+        />
         
-        {this.state.monsters.map((monster) => {
+        {filteredMonsters.map((monster) => {
             return <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>;
