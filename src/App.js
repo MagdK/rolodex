20e4bc31d20e8fcd33c9d515915906b4146b880e
MagdK <kovacsmagda@gmail.com>
2022-06-27 14:15:43 +0200

    Move search input tag to the component file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 
 import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -47,13 +48,12 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input 
-          className='search-box' 
-          type='serach' 
+       
+        <SearchBox 
+          onChangeHandler={onSearchChange} 
           placeholder='Search monsters...' 
-          onChange={onSearchChange}
+          className='serach-box'
         />
-        
         <CardList monsters={filteredMonsters} />
       </div>
     );
