f155be4bb58bb407666df83ad349edc45175216c
MagdK <kovacsmagda@gmail.com>
2022-06-28 19:41:21 +0200

    Fix CardList component return

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,11 +2,13 @@ import Card from '../card/card.component'
 import './card-list.styles.css';
 
 const CardList = ({ monsters }) => {
-    <div className='card-list'>
-        {monsters.map((monster) => {
-            return <Card monster={monster} />
-        })}
-    </div>
+    return (
+        <div className='card-list'>
+            {monsters.map((monster) => {
+                return <Card monster={monster} />
+            })}
+        </div>
+    )
 }
 
 export default CardList;
