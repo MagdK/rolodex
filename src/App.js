1e48a7c035c15aba71702cb24b0df03df716b9e7
MagdK <kovacsmagda@gmail.com>
2022-06-30 16:28:01 +0200

    Add title input to App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import './App.css';
 const App = () => {
   const [searchField, setSearchField] = useState('');
   // console.log({searchField}); // we are loggin it out as an object, but setState is not an object in our case, it is a string
+  const [title, setTitle] = useState('');
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
@@ -35,15 +36,26 @@ const App = () => {
     setSearchField(searchFieldString);
   };
 
+  const onTitleChange = (event) => { 
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setTitle(searchFieldString);
+  };
+
 
   
   return (
     <div className="App">
-      <h1 className='app-title'>Monsters Rolodex</h1>
+      <h1 className='app-title'>{title}</h1>
       <SearchBox 
-        className='monsters-serach-box'
+        className='monsters-search-box'
         onChangeHandler={onSearchChange} 
         placeholder='Search monsters...' 
+      />
+      <br />
+      <SearchBox 
+        className='title-search-box'
+        onChangeHandler={onTitleChange} 
+        placeholder='Set title...' 
       />
       <CardList monsters={filteredMonsters} />
     </div>
